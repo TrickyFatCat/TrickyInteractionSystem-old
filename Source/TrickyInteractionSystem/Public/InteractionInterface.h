fc8e83af3b95e6dc3cd93815f408c6f6dbb6d4c3
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-18 22:29:32 +0300

    Create an interaction interface

---
+++ Source/TrickyInteractionSystem/Public/InteractionInterface.h
@@ -0,0 +1,26 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InteractionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, BlueprintType)
+class UInteractionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class TRICKYINTERACTIONSYSTEM_API IInteractionInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Interaction")
+	bool ProcessInteraction(AActor* TargetActor);
+};
